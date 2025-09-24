becdbbaea30544394617ee55528d6e00a95783ef
Nisarul Amin Naim <65971192+nisarul-dev@users.noreply.github.com>
2025-09-24 22:43:10 +0600

    Why Online Counseling section ongoing

--- index.php
+++ index.php
@@ -45,23 +45,36 @@
             foreach( $slides as $slide ): 
             ?>
                 <div id="slide<?php echo esc_attr( $i ); ?>" class="carousel-item relative w-full">
-                    <div class="flex flex-col lg:flex-row">
-                        <div class="left-area w-[625px]">
-                            <h3>Why Choose Online Therapy?</h3>
-                            <p>
-                                In today’s fast-paced world, finding time for your mental well-being shouldn’t be difficult. Our online counseling sessions make it easy and safe to talk with a psychologist anytime, from anywhere. In today’s fast-paced world, finding time for your mental well-being shouldn’t be difficult. Our online counseling sessions make it easy and safe to talk with a psychologist anytime, from anywhere.
+                    <div class="flex flex-col justify-between lg:flex-row">
+                        <div class="left-area w-[48%]">
+                            <h3 class="text-font-gray text-4xl font-medium mb-[54px] mt-[52px]"><?php echo esc_html( $slide['heading'] ); ?></h3>
+                            <p class="text-font-gray text-lg/[32px] mb-[54px]">
+                                <?php echo esc_html( $slide['description'] ); ?>
                             </p>
+
                             <div class="navigation">
                                 <?php
                                     $prev = ( 0 >= ( $i - 1 ) ) ? count( $slides ) : $i - 1  ;
                                     $next = ( count( $slides ) >= ( $i + 1 ) ) ? $i + 1 :  1;
                                 ?>
-                                <a href="#slide<?php echo esc_attr( $prev ); ?>" class="btn btn-circle">❮</a>
-                                <a href="#slide<?php echo esc_attr( $next ); ?>" class="btn btn-circle">❯</a>
+                                <a href="#slide<?php echo esc_attr( $prev ); ?>" class="btn bg-white hover:bg-gray-200 py-[24px] px-[30px] rounded-[50px] border-black mr-[14px]">
+                                    <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.81299 7.72461L7.68799 0.849608C7.9458 0.591796 8.28955 0.46289 8.6333 0.46289C9.02002 0.46289 9.36377 0.591796 9.62158 0.849608C10.1802 1.36523 10.1802 2.26758 9.62158 2.7832L5.10986 7.33789L18.2583 7.33789C19.0317 7.33789 19.6333 7.93945 19.6333 8.71289C19.6333 9.44336 19.0317 10.0879 18.2583 10.0879L5.10986 10.0879L9.62158 14.5996C10.1802 15.1152 10.1802 16.0176 9.62158 16.5332C9.10596 17.0918 8.20361 17.0918 7.68799 16.5332L0.812989 9.6582C0.254396 9.14258 0.254396 8.24023 0.81299 7.72461Z" fill="black"/>
+                                    </svg>
+                                </a>
+                                <a href="#slide<?php echo esc_attr( $next ); ?>" class="btn bg-theme-blue hover:bg-theme-blue-hover py-[24px] px-[30px] rounded-[50px] border-theme-blue">
+                                    <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M19.2036 10.2012L12.3286 17.0762C12.0708 17.334 11.7271 17.4629 11.3833 17.4629C10.9966 17.4629 10.6528 17.334 10.395 17.0762C9.83643 16.5605 9.83643 15.6582 10.395 15.1426L14.9067 10.5879H1.7583C0.984863 10.5879 0.383301 9.98633 0.383301 9.21289C0.383301 8.48242 0.984863 7.83789 1.7583 7.83789H14.9067L10.395 3.32617C9.83643 2.81055 9.83643 1.9082 10.395 1.39258C10.9106 0.833984 11.813 0.833984 12.3286 1.39258L19.2036 8.26758C19.7622 8.7832 19.7622 9.68555 19.2036 10.2012Z" fill="white"/>
+                                    </svg>
+                                </a>
                             </div>
                         </div>
-                        <div class="right-area">
-                            <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" class="w-full" />
+                        <div class="right-area w-[50%] text-right">
+                            <?php
+                            echo wp_get_attachment_image( $slide['image'], 'full', false, array( 
+                                'class' => 'h-[580px] object-cover ml-auto mr-[20px] rounded-[20px]' 
+                            ) );
+                            ?>
                         </div>
                     </div>
                 </div>
